2f2c27b1438c8f70ac3f4bb10ab25c35631a4e51
terry-tupano <terrytupano@hotmail.com>
2026-04-25 08:20:56 +0200

    backup

--- src/main/java/de/simone/colortextfield/Utils.java
+++ src/main/java/de/simone/colortextfield/Utils.java
@@ -1,3 +1,22 @@
+/*-
+ * #%L
+ * TrendChart Add-on for vaadin Flow
+ * %%
+ * Copyright (C) 2026 Terry Tupano
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package de.simone.colortextfield;
 
 import java.util.ArrayList;
@@ -17,6 +36,39 @@ import mil.nga.color.ColorUtils;
  * ColorTextField.
  */
 public class Utils {
+
+    static String[] standardColors = { "#c00000", "#ff0000", "#ffc000", "#ffff00", "#92d050", "#00b050", "#00b0f0",
+            "#0070c0", "#002060", "#7030a0" };
+
+    static String[][] webColors = {
+            { "#003366", "#336699", "#3366cc", "#003399", "#000099", "#0000cc", "#000066" },
+            { "#006666", "#006699", "#0099cc", "#0066cc", "#0033cc", "#0000ff", "#3333ff", "#333399" },
+            { "#669999", "#009999", "#33cccc", "#00ccff", "#0099ff", "#0066ff", "#3366ff", "#3333cc", "#666699" },
+            { "#339966", "#00cc99", "#00ffcc", "#00ffff", "#33ccff", "#3399ff", "#6699ff", "#6666ff", "#6600ff",
+                    "#6600cc" },
+            { "#339933", "#00cc66", "#00ff99", "#66ffcc", "#66ffff", "#66ccff", "#99ccff", "#9999ff", "#9966ff",
+                    "#9933ff", "#9900ff" },
+            { "#006600", "#00cc00", "#00ff00", "#66ff99", "#99ffcc", "#ccffff", "#ccccff", "#cc99ff", "#cc66ff",
+                    "#cc33ff", "#cc00ff", "#9900cc" },
+            { "#003300", "#009933", "#33cc33", "#66ff66", "#99ff99", "#ccffcc", "#ffffff", "#ffccff", "#ff99ff",
+                    "#ff66ff", "#ff00ff", "#cc00cc", "#660066" },
+            { "#333300", "#009900", "#66ff33", "#99ff66", "#ccff99", "#ffffcc", "#ffcccc", "#ff99cc", "#ff66cc",
+                    "#ff33cc", "#cc0099", "#993399" },
+            { "#336600", "#669900", "#99ff33", "#ccff66", "#ffff99", "#ffcc99", "#ff9999", "#ff6699", "#ff3399",
+                    "#cc3399", "#990099" },
+            { "#666633", "#99cc00", "#ccff33", "#ffff66", "#ffcc66", "#ff9966", "#ff6666", "#ff0066", "#d60094",
+                    "#993366" },
+            { "#a58800", "#cccc00", "#ffff00", "#ffcc00", "#ff9933", "#ff6600", "#ff0033", "#cc0066", "#660033" },
+            { "#996633", "#cc9900", "#ff9900", "#cc6600", "#ff3300", "#ff0000", "#cc0000", "#990033" },
+            { "#663300", "#996600", "#cc3300", "#993300", "#990000", "#800000", "#993333" }
+    };
+    
+    static String[][] monoColors = {
+            { "#ffffff", "#ebebeb", "#d7d7d7", "#c3c3c3", "#afafaf", "#9b9b9b", "#878787", "#737373", "#5f5f5f",
+                    "#4b4b4b", "#373737", "#232323", "#0f0f0f" },
+            { "#f5f5f5", "#e1e1e1", "#cdcdcd", "#b9b9b9", "#a5a5a5", "#919191", "#7d7d7d", "#696969", "#555555",
+                    "#414141", "#2d2d2d", "#191919", "#050505" }
+    };
 
     /**
      * Converts an HSL color to its hexadecimal representation.
@@ -105,7 +157,7 @@ public class Utils {
     }
 
     /**
-     * Returns a map of web color names and their corresponding hex values.
+     * Returns a map of common web color names and their corresponding hex values.
      * 
      * @return the map
      */
@@ -188,4 +240,5 @@ public class Utils {
         webColors.put("YELLOWGREEN", "#9acd32");
         return webColors;
     }
+
 }
